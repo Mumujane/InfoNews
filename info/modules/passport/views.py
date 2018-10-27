1552158a940d5d8e9d269d1a11438029dd82d3e1
曾丽辉 <zenglihui@cenglihuideMacBook-Pro.local>
2018-10-27 19:09:58 +0800

    短信验证码

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -39,6 +39,7 @@ def get_img_code():
     response.content_type = "image/jpeg"
     return response
 
+
 @passport_blue.route("/get_sms_code", methods=['POST'])
 def get_sms_code():
     """
@@ -46,20 +47,43 @@ def get_sms_code():
     :return: json结果
     """
     # 获取参数
-
+    img_code_id = request.json.get("img_code_id")
+    img_code = request.json.get("img_code")
+    mobile = request.json.get("mobile")
 
     # 校验参数
+    if not all([img_code_id, img_code, mobile]):
+        return jsonify(errno=RET.PARAMERR, errmsg = error_map[RET.PARAMERR])
 
     # 校验手机格式
+    if not re.match(r"1[34567]\d{9}$",mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg = error_map[RET.PARAMERR])
 
     # 校验图片验证码 根据图片key取出真实的验证码文字
+    try:
+        real_img_code = sr.get("img_code_id_" + img_code_id)
+
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errmsg = error_map[RET.DBERR])
 
     # 生成随机短信验证码
+    rand_num = "%04d" % random.randint(0, 9999)
 
     # 打印验证码
+    current_app.logger.info(("短信验证码为:%s" % rand_num))
 
-    # 发送短信
+    # 发送短信(需要注册账户 配置信息)
+    # response_code = CCP().send_template_sms(mobile, [rand_num, 5], 1)
+    # if response_code != 0:
+    #     return jsonify(errno = RET.THIRDERR, errmsg = error_map[RET.THIRDERR])
 
     # 保存短信验证码 设置过期时间
+    try:
+        sr.set("sms_code_id_" + mobile, rand_num, ex = 60)
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno = RET.DBERR, errmsg = error_map[RET.DBERR])
 
     # 返回json结果
+    return jsonify(errno = RET.OK, errmsg = error_map[RET.OK])
